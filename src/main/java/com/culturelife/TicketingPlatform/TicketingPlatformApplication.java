44e70f346467808ebb077b07670ce4913548da1e
junoooooooooo <wnsgh732@gmail.com>
2024-03-26 22:02:30 +0900

    spring init

---
+++ src/main/java/com/culturelife/TicketingPlatform/TicketingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.culturelife.TicketingPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketingPlatformApplication.class, args);
+	}
+
+}
