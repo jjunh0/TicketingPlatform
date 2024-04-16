dc9130858ecf264ed00800d296aca1c9b5ac8f00
Siseum2 <minami1104@naver.com>
2024-04-17 01:06:58 +0900

    feat: 1) jpa 기능 추가(h2 db와 연동)           2) 각 테이블 생성(erd 구조 참고) 및 erd 수정사진 추가           3) Member, Question 엔티티 Repository와 Service 기능 추가           참고사항 DB 실행후 application 실행해야됨           / resources > H2 DB 생성 방법 및 접근방법 텍스트파일 반드시 확인

---
+++ src/main/java/com/culturelife/TicketingPlatform/Entity/Performance.java
@@ -0,0 +1,34 @@
+package com.culturelife.TicketingPlatform.Entity;
+
+import com.culturelife.TicketingPlatform.Entity.Enum.OrderStatus;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "performance")
+public class Performance {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "performance_id")
+    private Long id;
+
+    @Column(nullable = false, length = 100, unique = true)
+    private String performanceName;
+
+    @Lob
+    @Column(nullable = false)
+    private String performanceContents;
+
+    @Column(nullable = false)
+    private int performancePrice;
+
+    @Column(nullable = false)
+    private int performanceCount;
+
+    @ElementCollection
+    private List<String> performanceSeat;
+
+    private LocalDateTime performanceDate;
+}
--- src/test/java/com/culturelife/TicketingPlatform/TicketingPlatformApplicationTests.java
+++ src/test/java/com/culturelife/TicketingPlatform/TicketingPlatformApplicationTests.java
@@ -1,7 +1,12 @@
 package com.culturelife.TicketingPlatform;
 
+import com.culturelife.TicketingPlatform.Entity.Member;
+import com.culturelife.TicketingPlatform.Service.MemberService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
 
 @SpringBootTest
 class TicketingPlatformApplicationTests {
@@ -9,5 +14,24 @@ class TicketingPlatformApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+
+	@Autowired
+	private MemberService memberService;
+
+	@Test
+	void testJpa() {
+		Member member = new Member();
+		member.setMemberId("knu");
+		member.setMemberName("강원대생");
+		member.setPassword("123456");
+		member.setMemberEmail("knu@naver.com");
+		LocalDateTime dateTime = LocalDateTime.of(2024, 4, 14, 0, 0);
+		member.setMemberCreateDate(dateTime);
+		member.setUniversityAttendance(true);
+
+		memberService.createMember(member);
+
+	}
+
 
 }
