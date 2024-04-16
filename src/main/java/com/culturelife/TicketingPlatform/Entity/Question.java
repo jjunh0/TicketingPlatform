023861b062b57a26cece80e2039877dc87b24808
Siseum2 <minami1104@naver.com>
2024-04-17 01:06:58 +0900

    feat: 1) jpa 기능 추가(h2 db와 연동)           2) 각 테이블 생성(erd 구조 참고) 및 erd 수정사진 추가           3) Member, Question 엔티티 Repository와 Service 기능 추가           참고사항 DB 실행후 application 실행해야됨           / resources > H2 DB 생성 방법 및 접근방법 텍스트파일 반드시 확인

---
+++ src/main/java/com/culturelife/TicketingPlatform/Entity/Question.java
@@ -0,0 +1,30 @@
+package com.culturelife.TicketingPlatform.Entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "question")
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "question_id")
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String questionSubject;
+
+    @Column(length = 4000, nullable = false)
+    private String questionContents;
+
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    private LocalDateTime questionCreateDate;
+
+    private LocalDateTime questionUpdateDate;
+}
