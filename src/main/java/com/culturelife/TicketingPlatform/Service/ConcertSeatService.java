6da389f7a7ce3806798ae73e7e976d561ed86615
junho <wnsgh732@gmail.com>
2024-05-01 19:25:57 +0900

    feat: 콘서트 좌석 정보 서비스 추가

---
+++ src/main/java/com/culturelife/TicketingPlatform/Service/ConcertSeatService.java
@@ -0,0 +1,12 @@
+package com.culturelife.TicketingPlatform.Service;
+
+import jakarta.transaction.Transactional;
+
+import java.util.List;
+
+@Transactional
+public class ConcertSeatService {
+    public List<Boolean> getSeatInfo(Long id) {
+
+    }
+}
