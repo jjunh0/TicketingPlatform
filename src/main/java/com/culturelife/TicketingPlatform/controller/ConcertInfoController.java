3d2f9e1d7465ee8176e5120550112ece0d137888
junho <wnsgh732@gmail.com>
2024-05-01 19:25:27 +0900

    feat: 날짜선택 맵핑

--- src/main/java/com/culturelife/TicketingPlatform/controller/ConcertInfoController.java
+++ src/main/java/com/culturelife/TicketingPlatform/controller/ConcertInfoController.java
@@ -1,12 +1,30 @@
 package com.culturelife.TicketingPlatform.controller;
 
+import com.culturelife.TicketingPlatform.Service.ConcertSeatService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ConcertInfoController {
-    @GetMapping("ConcertInfo")
+
+//    private final ConcertSeatService concertSeatService;
+//    @Autowired
+//    public ConcertInfoController(ConcertSeatService concertSeatService) {
+//        this.concertSeatService = concertSeatService;
+//    }
+    @GetMapping("/concert-info")
     public String concertInfo() {
+        System.out.println("concert-info");
         return "concertinfo";
     }
+    @GetMapping("/concert-select")
+    @ResponseBody
+    public String concertSelect(@RequestParam Integer id) {
+        return Integer.toString(id);
+    }
+
 }
