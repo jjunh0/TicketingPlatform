2cc6fcf2a543338d97cf16823775ab0c06df6f3e
jjunh0 <94045552+jjunh0@users.noreply.github.com>
2024-05-08 21:19:44 +0900

    chore: 콘서트 정보 확인, 좌석 예매 기능 추가 (#8)
    * FEAT: ConcertInfoController 추가
    
    * CHORE: 로그인 페이지 제거
    
    * FEAT: 타임리프 추가
    
    * feat: 날짜선택 맵핑
    
    * feat: 콘서트 좌석 정보 서비스 추가
    
    * feat: 좌석 조회, 좌석 예매 기능 추가
    
    * feat: 콘서트 정보 조회, 콘서트 날짜 선택, 좌석 조회, 좌석 예매 기능 추가
    
    * feat: 좌석 정보, 좌석 선택 dto 추가
    
    * feat: PerformanceRepository 추가
    
    * feat: SeatRepository 추가
    
    * chore: api 경로 rest에 맞게 수정, 변수명 변경
    
    * chore: 변수명 변경
    
    * chore: 변수명 변경
    
    * feat: performanceDTO 추가
    
    * chore: performanceDTO를 사용하도록 변경
    
    * chore: seat Long type에서 String으로 수정
    
    * chore: package 경로 수정

---
+++ src/main/java/com/culturelife/TicketingPlatform/Controller/ConcertInfoController.java
@@ -0,0 +1,44 @@
+package com.culturelife.TicketingPlatform.Controller;
+
+import com.culturelife.TicketingPlatform.Service.ConcertSeatService;
+import com.culturelife.TicketingPlatform.Entity.dto.PerformanceDTO;
+import com.culturelife.TicketingPlatform.Entity.dto.SeatInfoDTO;
+import com.culturelife.TicketingPlatform.Entity.dto.SeatSelectionDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Comparator;
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class ConcertInfoController {
+
+    private final ConcertSeatService concertSeatService;
+    @GetMapping("/performances/{performanceId}")
+    public String concertInfo(@PathVariable("performanceId") Long performanceId, Model model) {
+        PerformanceDTO performanceDTO = concertSeatService.getPerformanceById(performanceId);
+        model.addAttribute("performance", performanceDTO);
+        return "concertInfo";
+    }
+    @GetMapping("/performances/{performanceId}/seats")
+    public String concertSelect(@PathVariable("performanceId") Long performanceId, Model model) {
+        List<SeatInfoDTO> seat = concertSeatService.getSeatById(performanceId);
+        model.addAttribute("seat", seat.stream().sorted(Comparator.comparing(SeatInfoDTO::getSeatName)));
+        return "seatSelect";
+    }
+
+    @PatchMapping("/seats/{performanceId}")
+    public String seatReserve(@PathVariable("performanceId") Long performanceId, SeatSelectionDTO seatSelectionForm) {
+        if(concertSeatService.reserve(performanceId, seatSelectionForm.getSeatName())) {
+            System.out.println("예약 성공");
+            return "redirect:/";
+        }
+        System.out.println("예약 실패");
+        return "redirect:/performances/" + performanceId + "/seats";
+    }
+
+
+}
