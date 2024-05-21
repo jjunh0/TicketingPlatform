f98dcfb072e9ca7c913be8b6f2fb45ec21bf02e2
belight2 <chlwnsgur090@gmail.com>
2024-05-21 16:42:47 +0900

    chore : 함수명 및 컨트롤러 내용 수정

--- src/main/java/com/culturelife/TicketingPlatform/Controller/CommunityController.java
+++ src/main/java/com/culturelife/TicketingPlatform/Controller/CommunityController.java
@@ -6,19 +6,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class CommunityController {
   // 질의 응답 게시판 글 목록으로 이동
-  @GetMapping("community/lists")
-  public String communityListsContoller(){
-    return "community/lists";
+  @GetMapping("community")
+  public String communityListsController(){
+    return "community";
   }
   // 질의 응답 글 작성 페이지로 이동
-  @GetMapping("community/write")
-  public String communityWriteContoller(){
-    return "community/write";
+  @GetMapping("createPost")
+  public String communityWriteController(){
+    return "createpost";
   }
   // 질의 응답 게시판의 작성된 글 페이지 보기
   // 이는 나중에 수정해야함
-  @GetMapping("community/view")
-  public String communityViewContoller(){
-    return "community/view";
-  }
+  @GetMapping("viewPoster")
+  public String communityViewController(){ return "post"; }
 }
