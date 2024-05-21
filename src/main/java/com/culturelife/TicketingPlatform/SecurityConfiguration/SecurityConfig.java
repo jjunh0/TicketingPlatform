5edf801777631a88d4198677e5bf914a5ba783d9
nawang311 <nawang311@gmail.com>
2024-05-21 19:16:03 +0900

    CORS, CSRF filter 코드 수정

--- src/main/java/com/culturelife/TicketingPlatform/SecurityConfiguration/SecurityConfig.java
+++ src/main/java/com/culturelife/TicketingPlatform/SecurityConfiguration/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
                     @Override
                     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                         CorsConfiguration config = new CorsConfiguration();
-                        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                        config.setAllowedOrigins(Collections.singletonList("http://localhost:8080"));
                         config.setAllowedMethods(Collections.singletonList("*"));
                         config.setAllowCredentials(true);
                         config.setAllowedHeaders(Collections.singletonList("*"));
