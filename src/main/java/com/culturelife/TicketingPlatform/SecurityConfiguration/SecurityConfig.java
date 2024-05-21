5a1b0fdebe72dd8d8b3bacfaf42aad9c0db5afc3
nawang311 <nawang311@gmail.com>
2024-05-22 00:32:59 +0900

    회원가입 Service 구현-보완 필요

--- src/main/java/com/culturelife/TicketingPlatform/SecurityConfiguration/SecurityConfig.java
+++ src/main/java/com/culturelife/TicketingPlatform/SecurityConfiguration/SecurityConfig.java
@@ -2,9 +2,12 @@ package com.culturelife.TicketingPlatform.SecurityConfiguration;
 
 import com.culturelife.TicketingPlatform.filter.CsrfCookieFilter;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -17,6 +20,8 @@ import org.springframework.web.cors.CorsConfigurationSource;
 
 import java.util.Collections;
 
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
@@ -39,19 +44,23 @@ public class SecurityConfig {
                         return config;
                     }
                 }))
-
-                /*
-                권한이 없는 사용자(비로그인) 접근을 허용할 페이지가 있을 경우
-                바로 아래 라인의 ignoringRequestMatchers 메소드 파라미터에 ("/", "/...") 형태로 추가해주시면 됩니다.
-                 */
-                .csrf((csrf) -> csrf.csrfTokenRequestHandler(requestHandler).ignoringRequestMatchers("/")
+                        /*
+                        권한이 없는 사용자(비로그인) 접근을 허용할 페이지가 있을 경우
+                        바로 아래 라인의 ignoringRequestMatchers 메소드 파라미터에 ("/", "/...") 형태로 추가해주시면 됩니다.
+                        */
+                .csrf((csrf) -> csrf.csrfTokenRequestHandler(requestHandler).ignoringRequestMatchers("/", "/h2-console/**")
                         .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+                .headers(headers -> headers
+                        .frameOptions(frameOptionsConfig -> frameOptionsConfig.disable())
+                )
                 .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
                 .authorizeHttpRequests((requests)->requests
 //                        .requestMatchers().authenticated()
-                        .requestMatchers("/", "/*", "/**").permitAll())
+                        .requestMatchers("/", "/**").permitAll())
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
+
+
         return http.build();
     }
 
