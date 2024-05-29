eaa04560db518d3bef0fcbec98a9dc74e3511b3f
junho <wnsgh732@gmail.com>
2024-05-29 15:43:24 +0900

    feat: 좌석 예약시 ticket, order 생성 기능 추가

--- src/main/java/com/culturelife/TicketingPlatform/Controller/ConcertInfoController.java
+++ src/main/java/com/culturelife/TicketingPlatform/Controller/ConcertInfoController.java
@@ -1,10 +1,13 @@
 package com.culturelife.TicketingPlatform.Controller;
 
+import com.culturelife.TicketingPlatform.Entity.Order;
 import com.culturelife.TicketingPlatform.Entity.dto.Message;
+import com.culturelife.TicketingPlatform.Service.AuthenticatedUserService;
 import com.culturelife.TicketingPlatform.Service.ConcertSeatService;
 import com.culturelife.TicketingPlatform.Entity.dto.PerformanceDTO;
 import com.culturelife.TicketingPlatform.Entity.dto.SeatInfoDTO;
 import com.culturelife.TicketingPlatform.Entity.dto.SeatSelectionDTO;
+import com.culturelife.TicketingPlatform.Service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,6 +22,8 @@ import java.util.List;
 public class ConcertInfoController {
 
     private final ConcertSeatService concertSeatService;
+    private final AuthenticatedUserService authenticatedUserService;
+    private final OrderService orderService;
     @GetMapping("/performances/{performanceId}")
     public String concertInfo(@PathVariable("performanceId") Long performanceId, Model model) {
         PerformanceDTO performanceDTO = concertSeatService.readPerformanceById(performanceId);
@@ -34,15 +39,18 @@ public class ConcertInfoController {
 
     @PostMapping("/seats/{performanceId}")
     public ModelAndView seatReserve(@PathVariable("performanceId") Long performanceId, SeatSelectionDTO seatSelectionForm, ModelAndView mav) {
-        if(concertSeatService.reserve(performanceId, seatSelectionForm.getSeatName())) {
-            mav.addObject("data", new Message("예약 성공.", "/performances/"+performanceId));
+        if(!concertSeatService.reserve(performanceId, seatSelectionForm.getSeatName())) {
+            System.out.println("예약 실패");
+            mav.addObject("data", new Message("이미 선택된 좌석입니다.", "/performances/"+performanceId+"/seats"));
             mav.setViewName("/common/message");
-            System.out.println("예약 성공");
             return mav;
         }
-        System.out.println("예약 실패");
-        mav.addObject("data", new Message("이미 선택된 좌석입니다.", "/performances/"+performanceId+"/seats"));
+        mav.addObject("data", new Message("예약 성공.", "/performances/"+performanceId));
         mav.setViewName("/common/message");
+
+        orderService.makeOrderAndTicket(performanceId, seatSelectionForm.getSeatName(),
+                authenticatedUserService.getCurrentMember().get());
+        System.out.println("예약 성공");
         return mav;
 
     }
