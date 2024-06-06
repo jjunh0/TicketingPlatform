742bbbdc4a7507a4840b0fa3cae4f159fb9eea7e
doublejh1417 <jaehun1417@gmail.com>
2024-06-06 19:53:18 +0900

    kakao->naver, 공연추가버튼, mypage css, contestlist페이지컨트롤러 수정

--- src/main/java/com/culturelife/TicketingPlatform/Controller/PerformanceController.java
+++ src/main/java/com/culturelife/TicketingPlatform/Controller/PerformanceController.java
@@ -4,6 +4,7 @@ import com.culturelife.TicketingPlatform.Entity.Performance;
 import com.culturelife.TicketingPlatform.Service.PerformanceService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,16 +19,24 @@ public class PerformanceController {
 
     private final PerformanceService performanceService;
 
-    @GetMapping(value = {"/contestlist/{page}", "/contestlist"})
-    public String list(Model model, @PathVariable(name="page", required = false) Integer page ) {
-        if(page == null) {
-            page = 1;
-        }
+    @GetMapping("/contestlist")
+    public String getContestList(Model model) {
+
+        return getContestListByPage(model, 1);
+    }
+
+    @GetMapping(value = {"/contestlist/page"})
+    public String getContestListByPage(Model model, @RequestParam(value = "page", defaultValue = "1") int page ) {
+
+
+        PageRequest pageable = PageRequest.of(page - 1, 10);
+        Page<Performance> performancePage = performanceService.readPostPage(page);
 
         Page<Performance> performances = performanceService.readPostPage(page);
         int totalPages = performances.getTotalPages();
         model.addAttribute("totalPages", totalPages);
         model.addAttribute("performancelist", performances);
+        model.addAttribute("currentPage", page);
 
         return "contestlist"; // 임시(수정필요)
     }
